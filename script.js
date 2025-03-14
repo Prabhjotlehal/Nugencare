49e8972a94e9593276b52dbc367bb739cc41d28f
Prabhjotlehal <prabhjotlehal01@gmail.com>
2025-03-14 15:50:09 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Example of GSAP animation for smooth scroll interaction
+gsap.from(".hero-text h1", { opacity: 0, y: -100, duration: 2 });
+gsap.from(".product", { opacity: 0, stagger: 0.2, y: 50, duration: 1 });
